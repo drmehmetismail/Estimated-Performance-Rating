27fc89369bf25524efb485705e359b173d140664
drmehmetismail <125666501+drmehmetismail@users.noreply.github.com>
2024-10-20 23:54:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from calculate_cpr import calculate_cpr
 from performance_rating_equilibrium import main_pre
 
 def main():
-    B = 2705 # Average rating of the opponents 2480, 3094
-    m, n = 20, 20 # Scoring m points in n games
+    B = 2705 # Average rating of the opponents
+    m, n = 11, 11 # Scoring m points in n games
     t = 0.75       # Threshold probability
-    # To calculat expected score between two players enter: A = 2800
+  
+    # To calculate expected score between two players enter: A = 2800
     # Calculate the win probability
     # print("Expected score:", calculate_win_probability(A, B))
-    # print(f"Expected score^{m}:", calculate_win_probability(A, B)**m)
 
     m, n = adjust_mn(m, n)
     w_star = optimize_w(m, n, t)
@@ -23,33 +23,33 @@ def main():
     FPR = calculate_FPR(m, n, B)
     FIDE_score_probability = calculate_score_probability(0.99, m, n)
 
-    # For Performance Rating Equilibrium (PRE) set csv_file_path and num_rounds
-    # csv_file_path = '.../GrandSwissPalma2017.csv'
-    # num_rounds = 9
+    # For Performance Rating Equilibrium (PRE) set pgn_input_dir and performance_rating_type
+    # performance_rating_type: input 'linear' or 'standard'. If it takes more than several minutes switch to linear as it's much faster.
+    # performance_rating_type = 'standard'
+    # pgn_input_dir = ''
     # main_pre(csv_file_path, num_rounds)
-  
+
     # w_star_plus = optimize_w_plus(m, n, t)
     # EPR_plus = calculate_EPR(w_star_plus, B)
     # score_plus_optimized_probability = calculate_score_plus_probability(w_star_plus, m, n)
 
     # Print B
-    print(f"B: {B}")
-    # Print m score in n games
+    print(f"Average rating of opponents: {B}")
+    # Print score m in n games
     print(f"Score: {m} / {n}")
-    # Print the threshold
-    # print(f"Threshold: {t}")
-    # Print w_star
-    print(f"w_star: {w_star}")
-    print(f"Optimized probability of scoring {m} points: {score_optimized_probability}")
-    print("FIDE score probability: ", FIDE_score_probability)
-    print("--------------------")
-    # calculate_score_probability w_star, m, n
-    # print(f"Score Probability: {calculate_score_probability(w, m, n)}")
     print(f"Estimated Performance Rating (EPR): {EPR}")
     print(f"Complete Performance Rating (CPR): {CPR}")
     print(f"Tournament Performance Rating (TPR): {TPR}")
     print("FIDE Performance Rating: ", FPR)
     print("--------------------")
+    # Advanced options
+    # print(f"Threshold: {t}")
+    # print(f"w_star: {w_star}")
+    # print(f"Optimized probability of scoring {m} points: {score_optimized_probability}")
+    # print("FIDE score probability: ", FIDE_score_probability)
+    # print("--------------------")
+    # calculate_score_probability w_star, m, n
+    # print(f"Score Probability: {calculate_score_probability(w, m, n)}")
     # print(f"w_star_plus: {w_star_plus}")
     # print(f"EPR_plus: {EPR_plus}")
     # print(f"Optimized probability of scoring {m} points or more: {score_plus_optimized_probability}")
